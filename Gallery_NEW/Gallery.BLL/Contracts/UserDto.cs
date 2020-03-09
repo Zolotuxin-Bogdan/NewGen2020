4503a8219defcabdbf610a8f5b136ef4ba4b6495
BOGDAN_ZOLOTUKHIN_KB18 <b.zolotuxin@gmail.com>
2020-03-09 17:11:36 +0200

    Created User Data Transfer Object

---
+++ Gallery_NEW/Gallery.BLL/Contracts/UserDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gallery.BLL.Contracts
+{
+    public class UserDTO
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+    }
+}
