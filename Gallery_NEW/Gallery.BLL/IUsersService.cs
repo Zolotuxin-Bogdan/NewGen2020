7003e07725347b709ab4d2adc90faa7aa7fa7163
BOGDAN_ZOLOTUKHIN_KB18 <b.zolotuxin@gmail.com>
2020-03-09 17:08:07 +0200

    Created UsersService and UsersService Interface

---
+++ Gallery_NEW/Gallery.BLL/IUsersService.cs
@@ -0,0 +1,16 @@
+﻿using Gallery.BLL.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gallery.BLL
+{
+    public interface IUsersService
+    {
+        Task<UserDTO> FindUserAsync(string username, string password);
+
+        Task<bool> IsUserExistAsync(string username, string password);
+    }
+}
---
+++ Gallery_NEW/Gallery.BLL/UsersService.cs
@@ -0,0 +1,25 @@
+﻿using Gallery.BLL.Contracts;
+using Gallery.DAL;
+using System;
+using System.Threading.Tasks;
+
+namespace Gallery.BLL
+{
+    public class UsersService : IUsersService
+    {
+        private readonly IRepository _repo;
+        public UsersService(IRepository repo)
+        {
+            _repo = repo ?? throw new ArgumentNullException(nameof(repo));
+        }
+        public async Task<bool> IsUserExistAsync(string username, string password)
+        {
+            return await _repo.IsUserExistAsync(username, password);
+        }
+        public Task<UserDTO> FindUserAsync(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
