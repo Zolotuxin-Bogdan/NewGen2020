6737d549cd470ebf8ba5ec666535378b95f95115
BOGDAN_ZOLOTUKHIN_KB18 <b.zolotuxin@gmail.com>
2020-03-09 17:09:38 +0200

    Created UsersRepository and Repository Interface

---
+++ Gallery_NEW/Gallery.DAL/UsersRepository.cs
@@ -0,0 +1,22 @@
+﻿using Gallery.DAL.Model;
+using System;
+using System.Data.Entity;
+using System.Threading.Tasks;
+
+namespace Gallery.DAL
+{
+    public class UsersRepository : IRepository
+    {
+        private readonly UserContext _ctx;
+
+        public UsersRepository(UserContext ctx)
+        {
+            _ctx = ctx ?? throw new ArgumentNullException(nameof(ctx));
+        }
+        public async Task<bool> IsUserExistAsync(string username, string password)
+        {
+            return await _ctx.Users.AnyAsync(u => u.EMail == username.Trim().ToLower() &&
+                                             u.Password == username.Trim());
+        }
+    }
+}
