c6a1af431fb23b0fd35183d6589d17057d7509ab
BOGDAN_ZOLOTUKHIN_KB18 <b.zolotuxin@gmail.com>
2020-03-09 13:26:47 +0200

    Created LoginModel and RegisterModel

---
+++ Gallery_NEW/Gallery.DAL/Model/Models.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gallery.DAL.Model
+{
+    public class LoginModel
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+
+    public class RegisterModel
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+}
