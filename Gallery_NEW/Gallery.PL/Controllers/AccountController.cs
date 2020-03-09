809cb6db538b14aa2d405b08514115e1715ab5d0
BOGDAN_ZOLOTUKHIN_KB18 <b.zolotuxin@gmail.com>
2020-03-09 23:34:19 +0200

    Created AccountController

---
+++ Gallery_NEW/Gallery.PL/Controllers/AccountController.cs
@@ -0,0 +1,96 @@
+﻿using Gallery.BLL;
+using Gallery.DAL;
+using Gallery.DAL.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Gallery.PL.Controllers
+{
+    public class AccountController : Controller
+    {
+        private IUsersService _usersService;
+        public AccountController(IUsersService usersService)
+        {
+            _usersService = usersService ?? throw new ArgumentNullException(nameof(usersService));
+        }
+
+        public AccountController() : this(new UsersService(new UsersRepository(new UserContext()))) { }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var canAuthorize = await _usersService.IsUserExistAsync(model.Name, model.Password);
+
+                if (canAuthorize)
+                {
+                    FormsAuthentication.SetAuthCookie(model.Name, true);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "User not found");
+                }
+            }
+            return View(model);
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var isUserExist = await _usersService.IsUserExistAsync(model.Name, model.Password);
+
+                if (isUserExist == false)
+                {
+                    
+                    // Register New User into Database
+                    using (UserContext database = new UserContext())
+                    {
+                        /* RegisterUserToDatabase(model.Name, model.Password) */
+
+                        // Exchange with top sentence
+                        database.Users.Add(new User { EMail = model.Name, Password = model.Password });
+                        database.SaveChanges();
+                        //
+
+                    }
+
+                    FormsAuthentication.SetAuthCookie(model.Name, true);
+                    return RedirectToAction("Index", "Home");
+
+                }
+                else
+                {
+                    ModelState.AddModelError("", "User already exists");
+                }
+            }
+            return View(model);
+        }
+
+        public ActionResult Logout()
+        {
+            return View();
+        }
+
+    }
+}
