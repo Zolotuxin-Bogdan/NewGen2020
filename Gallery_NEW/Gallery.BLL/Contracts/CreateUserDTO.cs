a03b65a616ebbcefe31848307bcf96acf51e92e7
BOGDAN_ZOLOTUKHIN_KB18 <b.zolotuxin@gmail.com>
2020-03-12 19:01:50 +0200

    Added CreateUserDTO

---
+++ Gallery_NEW/Gallery.BLL/Contracts/CreateUserDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gallery.BLL.Contracts
+{
+    public class CreateUserDTO
+    {
+        public CreateUserDTO(string email, string password)
+        {
+            EMail = email;
+            Password = password;
+        }
+        public string EMail { get; }
+        public string Password { get; set; }
+    }
+}
