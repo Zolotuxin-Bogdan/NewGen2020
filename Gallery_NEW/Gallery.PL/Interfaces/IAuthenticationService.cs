ca62e41cdeaa37d4b8dd29b056d28433a35d3be4
BOGDAN_ZOLOTUKHIN_KB18 <b.zolotuxin@gmail.com>
2020-03-23 20:47:48 +0200

    Implemented Authentication Service and his Interface in Gallery.PL

---
+++ Gallery_NEW/Gallery.PL/Interfaces/IAuthenticationService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using Microsoft.Owin;
+
+namespace Gallery.PL.Interfaces
+{
+    public interface IAuthenticationService
+    {
+        void AuthByOwinCookies(IOwinContext owinCtx, ClaimsIdentity claim);
+
+        ClaimsIdentity ClaimsСreation(string claims);
+    }
+}
---
+++ Gallery_NEW/Gallery.PL/Services/AuthenticationService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using Gallery.PL.Interfaces;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+
+namespace Gallery.PL.Services
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        public void AuthByOwinCookies(IOwinContext owinCtx, ClaimsIdentity claim)
+        {
+            owinCtx.Authentication.SignOut();
+            owinCtx.Authentication.SignIn(new AuthenticationProperties
+            {
+                IsPersistent = true
+            }, claim);
+        }
+
+        public ClaimsIdentity ClaimsСreation(string claims)
+        {
+            ClaimsIdentity claim = new ClaimsIdentity("ApplicationCookie", ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
+            claim.AddClaim(new Claim(ClaimTypes.NameIdentifier, claims, ClaimValueTypes.String));
+            return claim;
+        }
+    }
+}
