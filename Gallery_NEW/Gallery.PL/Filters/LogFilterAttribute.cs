6192fedd387e71a1d31f3a16ff5225f6af287ed5
BOGDAN_ZOLOTUKHIN_KB18 <b.zolotuxin@gmail.com>
2020-04-13 17:53:10 +0300

    Implemented OnActionExecuting and OnActionExecuted method for LogFilter

---
+++ Gallery_NEW/Gallery.PL/Filters/LogFilterAttribute.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NLog;
+
+namespace Gallery.PL.Filters
+{
+    public class LogFilterAttribute : ActionFilterAttribute
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var request = filterContext.HttpContext.Request;
+            
+            var ipAddress = request.ServerVariables["HTTP_X_FORWARDED_FOR"] ?? request.UserHostAddress;
+            var dnsName = request.UserHostName;
+            var machineName = filterContext.HttpContext.Server.MachineName;
+            var rawUrl = request.RawUrl;
+            var userAgent = request.UserAgent;
+            var httpMethod = request.HttpMethod;
+
+            string messageRequest = "\n<<<\n[Request]" +
+                                    "\nIP: " + ipAddress + 
+                                    "\nDNS-name: " + dnsName + 
+                                    "\nMachineName: " + machineName + 
+                                    "\nURL-address: " + rawUrl + 
+                                    "\nUserAgent: " + userAgent +
+                                    "\nHttpMethod: " + httpMethod + "\n>>>\n";
+            Logger.Info(messageRequest);
+
+            base.OnActionExecuting(filterContext);
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            var response = filterContext.HttpContext.Response;
+
+            var status = response.Status;
+
+            string messageResponse = "\n<<<\n[Response]" +
+                                     "\nStatus: " + status + "\n>>>\n";
+            Logger.Info(messageResponse);
+            base.OnActionExecuted(filterContext);
+        }
+    }
+}
