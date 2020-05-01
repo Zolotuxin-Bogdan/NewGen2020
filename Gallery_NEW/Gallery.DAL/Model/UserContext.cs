097044555f1e4b011f3e518a064e2c9852e1c09c
BOGDAN_ZOLOTUKHIN_KB18 <b.zolotuxin@gmail.com>
2020-05-01 16:07:19 +0300

    Created UserDbInitializer which create admin user and create 2 roles: 'admin' and 'user'

--- Gallery_NEW/Gallery.DAL/Model/UserContext.cs
+++ Gallery_NEW/Gallery.DAL/Model/UserContext.cs
@@ -12,5 +12,26 @@ namespace Gallery.DAL.Model
         public UserContext(string connectionString) : base(connectionString) { }
 
         public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+    }
+
+    public class UserDbInitializer : DropCreateDatabaseAlways<UserContext>
+    {
+        protected override void Seed(UserContext ctx)
+        {
+            User adminUser = new User { Id = 1, EMail = "admin@admin.admin", Password = "admin" };
+
+            ctx.Users.AddRange(new List<User> { adminUser });
+            ctx.SaveChanges();
+
+            Role role1 = new Role { Name = "admin" };
+            Role role2 = new Role { Name = "user" };
+
+            role1.Users.Add(adminUser);
+
+            ctx.Roles.AddRange(new List<Role> { role1, role2});
+            ctx.SaveChanges();
+
+        }
     }
 }
