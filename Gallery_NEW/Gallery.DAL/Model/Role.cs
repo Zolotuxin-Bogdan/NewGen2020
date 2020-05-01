2244187e239bac8fa0b07dfde9c151e44c194aa8
BOGDAN_ZOLOTUKHIN_KB18 <b.zolotuxin@gmail.com>
2020-05-01 14:45:54 +0300

    Created new Role model

---
+++ Gallery_NEW/Gallery.DAL/Model/Role.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gallery.DAL.Model
+{
+    public class Role
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<User> Users { get; set; }
+        public Role()
+        {
+            Users = new List<User>();
+        }
+    }
+}
