1c429a4b3b23242119d89a484d12b473c2f87684
BOGDAN_ZOLOTUKHIN_KB18 <b.zolotuxin@gmail.com>
2020-05-01 17:43:13 +0300

    Created new MediaType model

---
+++ Gallery_NEW/Gallery.DAL/Model/MediaType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gallery.DAL.Model
+{
+    public enum TypeOfMedia
+    {
+        Image, Video, Sound
+    }
+    public class MediaType
+    {
+        public int Id { get; set; }
+        public TypeOfMedia Type { get; set; }
+
+
+        public ICollection<Media> Medias { get; set; }
+        public MediaType()
+        {
+            Medias = new List<Media>();
+        }
+    }
+}
