2c1ed97483d9da24355f620a303f2e10a7a101fe
BOGDAN_ZOLOTUKHIN_KB18 <b.zolotuxin@gmail.com>
2020-05-01 19:59:54 +0300

    Removed enum MediaType

--- Gallery_NEW/Gallery.DAL/Model/MediaType.cs
+++ Gallery_NEW/Gallery.DAL/Model/MediaType.cs
@@ -6,14 +6,10 @@ using System.Threading.Tasks;
 
 namespace Gallery.DAL.Model
 {
-    public enum TypeOfMedia
-    {
-        Image, Video, Sound
-    }
     public class MediaType
     {
         public int Id { get; set; }
-        public TypeOfMedia Type { get; set; }
+        public string Type { get; set; }
 
 
         public ICollection<Media> Medias { get; set; } = 
