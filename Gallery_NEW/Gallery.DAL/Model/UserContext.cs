0e67096ff7f4a9bb74f6796e1135d7c7764b4806
BOGDAN_ZOLOTUKHIN_KB18 <b.zolotuxin@gmail.com>
2020-05-07 21:07:38 +0300

    Revoved DropCreateDatabase method from UserContext

--- Gallery_NEW/Gallery.DAL/Model/UserContext.cs
+++ Gallery_NEW/Gallery.DAL/Model/UserContext.cs
@@ -18,23 +18,5 @@ namespace Gallery.DAL.Model
 
     }
 
-    public class UserDbInitializer : DropCreateDatabaseAlways<UserContext>
-    {
-        protected override void Seed(UserContext ctx)
-        {
-            User adminUser = new User { Id = 1, Email = "admin@admin.admin", Password = "admin" };
-
-            ctx.Users.AddRange(new List<User> { adminUser });
-            ctx.SaveChanges();
-
-            Role role1 = new Role { Name = "admin" };
-            Role role2 = new Role { Name = "user" };
-
-            role1.Users.Add(adminUser);
-
-            ctx.Roles.AddRange(new List<Role> { role1, role2});
-            ctx.SaveChanges();
-
-        }
-    }
+    
 }
