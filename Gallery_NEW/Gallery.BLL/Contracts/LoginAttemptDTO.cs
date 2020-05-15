f06de3b59b6e7e3bae5f79df3c1c607913fb16ca
BOGDAN_ZOLOTUKHIN_KB18 <b.zolotuxin@gmail.com>
2020-05-15 15:08:42 +0300

    Created LoginAttemptDTO

---
+++ Gallery_NEW/Gallery.BLL/Contracts/LoginAttemptDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gallery.BLL.Contracts
+{
+    public class LoginAttemptDTO
+    {
+        public LoginAttemptDTO(string email, string ipAddress, bool isSuccess)
+        {
+            Email = email;
+            IpAddress = ipAddress;
+            IsSuccess = isSuccess;
+        }
+
+        public string Email { get; }
+        public string IpAddress { get; set; }
+        public bool IsSuccess { get; set; }
+    }
+}
