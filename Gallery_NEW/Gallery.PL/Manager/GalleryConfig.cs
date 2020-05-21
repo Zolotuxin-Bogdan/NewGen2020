f3c63a6ab38f276af09a1be5a131d527ef5e2bae
BOGDAN_ZOLOTUKHIN_KB18 <b.zolotuxin@gmail.com>
2020-05-21 17:20:42 +0300

    Implemented GalleryConfig manager

--- Gallery_NEW/Gallery.PL/Manager/GalleryConfig.cs
+++ Gallery_NEW/Gallery.PL/Manager/GalleryConfig.cs
@@ -1,12 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Configuration;
 
 namespace Gallery.PL.Manager
 {
     public static class GalleryConfig
     {
+        public static string key_pathForSave { get; } = "PathForSave";
+        public static string key_imageTypes { get; } = "SupportedPhotoFormat";
 
+        private const string default_pathForSave = "/Images/Upload/";
+        private const string default_imageTypes = "image/jpeg; image/png";
+
+        public static string GetPathForSave()
+        {
+            var appSettings = ConfigurationManager.AppSettings;
+            var path = default_pathForSave;
+            if (!string.IsNullOrEmpty(appSettings[key_pathForSave]))
+            {
+                 path = appSettings[key_pathForSave];
+            }
+            return path;
+        }
+
+        public static string GetImageTypes()
+        {
+            var appSettings = ConfigurationManager.AppSettings;
+            var imageTypes = default_imageTypes;
+            if (!string.IsNullOrEmpty(appSettings[key_imageTypes]))
+            {
+                imageTypes = appSettings[key_pathForSave];
+            }
+            return imageTypes;
+        }
+
+        public static string GetDbConnectionString()
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["sql"] ?? throw new ArgumentException("SQL");
+            return connectionString.ConnectionString;
+        }
     }
 }
