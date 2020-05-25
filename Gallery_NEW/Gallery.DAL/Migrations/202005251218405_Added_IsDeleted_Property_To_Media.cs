cd167be74cafe996a825a329b569ea4c91c2ea89
BOGDAN_ZOLOTUKHIN_KB18 <b.zolotuxin@gmail.com>
2020-05-25 15:22:21 +0300

    Added new migration IsDeleted column added to Media model

---
+++ Gallery_NEW/Gallery.DAL/Migrations/202005251218405_Added_IsDeleted_Property_To_Media.cs
@@ -0,0 +1,18 @@
+﻿namespace Gallery.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Added_IsDeleted_Property_To_Media : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("Gallery.Media", "IsDeleted", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("Gallery.Media", "IsDeleted");
+        }
+    }
+}
