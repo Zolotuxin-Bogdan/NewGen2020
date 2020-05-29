d582f00ba7f8039df28833d01b7b7c7201b04d5f
BOGDAN_ZOLOTUKHIN_KB18 <b.zolotuxin@gmail.com>
2020-05-29 16:25:27 +0300

    Implemented MSMQPublisher via IPublisher

---
+++ Gallery_NEW/Gallery.PL/Interfaces/IPublisher.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Gallery.PL.Interfaces
+{
+    public interface IPublisher
+    {
+        void SendMessage(object message, string label);
+    }
+}
---
+++ Gallery_NEW/Gallery.PL/Services/MSMQPublisher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Gallery.PL.Interfaces;
+using System.Messaging;
+
+namespace Gallery.PL.Services
+{
+    public class MsmqPublisher : IPublisher
+    {
+        private readonly MessageQueue _messageQueue;
+
+        public MsmqPublisher(MessageQueue messageQueue)
+        {
+            _messageQueue = messageQueue ?? throw new ArgumentNullException(nameof(messageQueue));
+        }
+
+        public void SendMessage(object message, string label)
+        {
+            _messageQueue.Send(message, label);
+        }
+    }
+}
