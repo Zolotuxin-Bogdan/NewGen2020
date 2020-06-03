a2668c74f6df15e5b5b91f64d1d1d3b88176d519
BOGDAN_ZOLOTUKHIN_KB18 <b.zolotuxin@gmail.com>
2020-06-03 20:00:02 +0300

    Implemented GetTempPath for saving media in GalleryConfig

--- Gallery_NEW/Gallery.PL/Manager/GalleryConfig.cs
+++ Gallery_NEW/Gallery.PL/Manager/GalleryConfig.cs
@@ -6,10 +6,12 @@ namespace Gallery.PL.Manager
     public static class GalleryConfig
     {
         public static string key_pathForSave { get; } = "PathForSave";
+        public static string key_tempPath { get; } = "TempPath";
         public static string key_imageTypes { get; } = "SupportedPhotoFormat";
         public static string key_messageQueuePath { get; } = "MessageQueuePath";
 
         private const string default_pathForSave = "/Images/Upload/";
+        private const string default_tempPath = "/Temp";
         private const string default_imageTypes = "image/jpeg; image/png";
 
         public static string GetPathForSave()
@@ -19,6 +21,17 @@ namespace Gallery.PL.Manager
             if (!string.IsNullOrEmpty(appSettings[key_pathForSave]))
             {
                  path = appSettings[key_pathForSave];
+            }
+            return path;
+        }
+
+        public static string GetTempPath()
+        {
+            var appSettings = ConfigurationManager.AppSettings;
+            var path = default_tempPath;
+            if (!string.IsNullOrEmpty(appSettings[key_tempPath]))
+            {
+                path = appSettings[key_tempPath];
             }
             return path;
         }
