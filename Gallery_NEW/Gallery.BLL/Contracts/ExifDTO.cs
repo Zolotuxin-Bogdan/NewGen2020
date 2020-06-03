e1f522cb6890b5f0e3a7d5a07a24c4645f900276
BOGDAN_ZOLOTUKHIN_KB18 <b.zolotuxin@gmail.com>
2020-06-03 20:12:20 +0300

    Implemented ExifDTO

--- Gallery_NEW/Gallery.BLL/Contracts/ExifDTO.cs
+++ Gallery_NEW/Gallery.BLL/Contracts/ExifDTO.cs
@@ -8,6 +8,11 @@ namespace Gallery.BLL.Contracts
 {
     public class ExifDTO
     {
-        
+        public string Title { get; set; }
+        public string Manufacturer { get; set; }
+        public string ModelOfCamera { get; set; }
+        public string FileSize { get; set; }
+        public string DateCreation { get; set; }
+        public string DateUpload { get; set; }
     }
 }
