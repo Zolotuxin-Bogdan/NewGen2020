2ba0e4efc5d51b28df1454005626a189d336e2f9
BOGDAN_ZOLOTUKHIN_KB18 <b.zolotuxin@gmail.com>
2020-06-05 14:35:40 +0300

    Added new migration Media_Name_Lenght_Changed_Migration

---
+++ Gallery_NEW/Gallery.DAL/Migrations/202006051122169_Media_Name_Lenght_Changed_Migration.cs
@@ -0,0 +1,18 @@
+﻿namespace Gallery.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Media_Name_Lenght_Changed_Migration : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("Gallery.Media", "Name", c => c.String(nullable: false, maxLength: 255, unicode: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("Gallery.Media", "Name", c => c.String(nullable: false, maxLength: 50, unicode: false));
+        }
+    }
+}
