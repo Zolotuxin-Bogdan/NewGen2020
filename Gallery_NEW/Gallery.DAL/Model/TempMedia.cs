20c7885ae9290f6977aad24a46753bb5f60cf86a
BOGDAN_ZOLOTUKHIN_KB18 <b.zolotuxin@gmail.com>
2020-06-12 21:20:44 +0300

    Implemented TempMedia model

--- Gallery_NEW/Gallery.DAL/Model/TempMedia.cs
+++ Gallery_NEW/Gallery.DAL/Model/TempMedia.cs
@@ -6,7 +6,14 @@ using System.Threading.Tasks;
 
 namespace Gallery.DAL.Model
 {
-    class TempMedia
+    public class TempMedia
     {
+        public int Id { get; set; }
+        public string UniqName { get; set; }
+        public bool IsLoading { get; set; }
+        public bool IsSuccess { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
     }
 }
