b040d266d112077784f87ae06592f522a6157691
BOGDAN_ZOLOTUKHIN_KB18 <b.zolotuxin@gmail.com>
2020-06-12 21:41:26 +0300

    Added TempMedia DbSet and TempMediaConfiguration to GalleryContext

--- Gallery_NEW/Gallery.DAL/Model/GalleryContext.cs
+++ Gallery_NEW/Gallery.DAL/Model/GalleryContext.cs
@@ -11,6 +11,7 @@ namespace Gallery.DAL.Model
         public DbSet<User> Users { get; set; }
         public DbSet<Role> Roles { get; set; }
         public DbSet<Media> Media { get; set; }
+        public DbSet<TempMedia> TempMedia { get; set; }
         public DbSet<MediaType> MediaType { get; set; }
         public DbSet<LoginAttempt> Attempts { get; set; }
 
@@ -22,6 +23,7 @@ namespace Gallery.DAL.Model
             modelBuilder.Configurations.Add(new UserConfiguration());
             modelBuilder.Configurations.Add(new RoleConfiguration());
             modelBuilder.Configurations.Add(new MediaConfiguration());
+            modelBuilder.Configurations.Add(new TempMediaConfiguration());
             modelBuilder.Configurations.Add(new MediaTypeConfiguration());
             modelBuilder.Configurations.Add(new AttemptLoginConfiguration());
 
