6a3e6ff9eaa6f15100e55487f598dd0f7cc92c36
BOGDAN_ZOLOTUKHIN_KB18 <b.zolotuxin@gmail.com>
2020-06-12 21:34:32 +0300

    Implemented TempMediaConfiguration class

--- Gallery_NEW/Gallery.DAL/Model/Configuration/TempMediaConfiguration.cs
+++ Gallery_NEW/Gallery.DAL/Model/Configuration/TempMediaConfiguration.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Gallery.DAL.Model.Configuration
 {
-    class TempMediaConfiguration
+    public class TempMediaConfiguration : EntityTypeConfiguration<TempMedia>
     {
+        public TempMediaConfiguration()
+        {
+            ToTable("TempMedia").HasKey(p => p.Id).Property(p => p.Id).IsRequired().HasColumnName("TempMediaId");
+
+            Property(p => p.UniqName).IsRequired().HasColumnType("varchar").HasMaxLength(255);
+            Property(p => p.IsLoading).IsRequired();
+            Property(p => p.IsSuccess).IsRequired();
+
+            HasRequired(p => p.User)
+                .WithMany(d => d.TempMedia)
+                .HasForeignKey(c => c.UserId);
+        }
     }
 }
