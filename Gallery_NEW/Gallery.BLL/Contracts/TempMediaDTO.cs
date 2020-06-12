b1f204ad931aea031ad56a27dfb80c5c901abdf7
BOGDAN_ZOLOTUKHIN_KB18 <b.zolotuxin@gmail.com>
2020-06-12 22:07:40 +0300

    Created and Implemented TempMediaDTO

---
+++ Gallery_NEW/Gallery.BLL/Contracts/TempMediaDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gallery.BLL.Contracts
+{
+    public class TempMediaDTO
+    {
+        public int Id { get; set; }
+        public string UniqName { get; set; }
+        public bool IsLoading { get; set; }
+        public bool IsSuccess { get; set; }
+        public int UserId { get; set; }
+    }
+}
