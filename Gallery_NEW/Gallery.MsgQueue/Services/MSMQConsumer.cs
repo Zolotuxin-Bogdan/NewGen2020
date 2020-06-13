a4e2a48ff3f722cbda9a3e28099980fec73266ae
BOGDAN_ZOLOTUKHIN_KB18 <b.zolotuxin@gmail.com>
2020-06-13 14:01:47 +0300

    Implemented MSMQConsumer

---
+++ Gallery_NEW/Gallery.MsgQueue/Services/MSMQConsumer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Messaging;
+using Gallery.MsgQueue.Interfaces;
+
+namespace Gallery.MsgQueue.Services
+{
+    public class MSMQConsumer : IConsumer
+    {
+        private readonly MessageQueue _messageQueue;
+        private readonly string _messageQueuePath;
+
+        public MSMQConsumer(string messageQueuePath)
+        {
+            _messageQueuePath = messageQueuePath ?? throw new ArgumentNullException(nameof(messageQueuePath));
+            _messageQueue = new MessageQueue(_messageQueuePath);
+
+            if (!MessageQueue.Exists(_messageQueue.Path))
+            {
+                MessageQueue.Create(_messageQueue.Path);
+            }
+
+        }
+        public object ReceiveFirstMessageBody()
+        {
+            return _messageQueue.Receive().Body;
+        }
+    }
+}
