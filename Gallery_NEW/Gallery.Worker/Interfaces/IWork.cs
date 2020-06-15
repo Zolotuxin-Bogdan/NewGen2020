6437a34773bce1f5aebbeb70b98fb94b23b715b7
BOGDAN_ZOLOTUKHIN_KB18 <b.zolotuxin@gmail.com>
2020-06-15 16:23:44 +0300

    Created and Implemented IWork interface in Gallery.Worker project

---
+++ Gallery_NEW/Gallery.Worker/Interfaces/IWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gallery.Worker.Interfaces
+{
+    public interface IWork
+    {
+        Task StartAsync();
+
+        Task StopAsync();
+    }
+}
