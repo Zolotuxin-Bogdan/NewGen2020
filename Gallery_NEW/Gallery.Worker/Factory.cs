41b2ad149c6ba49c2f81efc6ebda3b42562ee4da
BOGDAN_ZOLOTUKHIN_KB18 <b.zolotuxin@gmail.com>
2020-06-15 15:49:30 +0300

    Implemented Factory class in Gallery.Worker project

--- Gallery_NEW/Gallery.Worker/Factory.cs
+++ Gallery_NEW/Gallery.Worker/Factory.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Gallery.Worker
 {
-    public class Factory
+    public static class Factory
     {
+        public static ConsumerConfiguration ParseConsumerConfiguration()
+        {
+            var path = ConfigurationManager.AppSettings["msmq:path"] ?? @".\private$\MQ";
+
+            return new ConsumerConfiguration(
+                path: @".\private$\MQ");
+        }
+    }
+
+    public class ConsumerConfiguration
+    {
+        public string Path { get; }
+
+        public ConsumerConfiguration(string path)
+        {
+            Path = path ?? throw new ArgumentNullException(nameof(path));
+        }
     }
 }
