463e0bd07268049796f23c869bf99996daf49aae
BOGDAN_ZOLOTUKHIN_KB18 <b.zolotuxin@gmail.com>
2020-06-19 14:08:57 +0300

    Created and Implemented 'MessageDTO'

---
+++ Gallery_NEW/Gallery.BLL/Contracts/MessageDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gallery.BLL.Contracts
+{
+    public class MessageDTO
+    {
+        public int UserId;
+        public string FileName;
+        public byte[] FileBytes;
+        public string MainPath;
+        public string TempPath;
+
+    }
+}
