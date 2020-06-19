b54adca323dd6d760d3b9a366de3e64ecec03827
BOGDAN_ZOLOTUKHIN_KB18 <b.zolotuxin@gmail.com>
2020-06-19 20:26:49 +0300

    Implemented 'WorkerWrapper' in Gallery.Worker

--- Gallery_NEW/Gallery.Worker/WorkerWrapper.cs
+++ Gallery_NEW/Gallery.Worker/WorkerWrapper.cs
@@ -2,11 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Gallery.Worker.Interfaces;
 
 namespace Gallery.Worker
 {
-    class WorkerWrapper
+    public class WorkerWrapper
     {
+        private readonly IReadOnlyCollection<IWork> _works;
+        private readonly CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
+
+        public WorkerWrapper(IReadOnlyCollection<IWork> works)
+        {
+            _works = works ?? throw new ArgumentNullException(nameof(works));
+        }
+
+        public async Task StartAsync()
+        {
+            foreach (var work in _works)
+            {
+                await Task.Factory.StartNew(work.StartAsync,
+                    _cancellationTokenSource.Token,
+                    TaskCreationOptions.LongRunning,
+                    TaskScheduler.Current);
+            }
+        }
+
+        public void Stop()
+        {
+            _cancellationTokenSource.Cancel();
+        }
     }
 }
