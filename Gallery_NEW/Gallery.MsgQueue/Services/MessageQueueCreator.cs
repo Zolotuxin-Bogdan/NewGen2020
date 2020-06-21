864c1b0ddaad40a4cd8678a76dd7e1225db59458
BOGDAN_ZOLOTUKHIN_KB18 <b.zolotuxin@gmail.com>
2020-06-21 23:59:12 +0300

    Implemented MessageQueueCreator

--- Gallery_NEW/Gallery.MsgQueue/Services/MessageQueueCreator.cs
+++ Gallery_NEW/Gallery.MsgQueue/Services/MessageQueueCreator.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Gallery.MsgQueue.Services
 {
-    class MessageQueueCreator
+    public static class MessageQueueCreator
     {
+        public static void CreateMessageQueues(List<string> msgQueueList)
+        {
+            foreach (var msg in msgQueueList)
+            {
+                if (string.IsNullOrWhiteSpace(msg))
+                {
+                    throw new ArgumentNullException(nameof(msg));
+                }
+
+                if (!MessageQueue.Exists(msg))
+                {
+                    MessageQueue.Create(msg);
+                }
+            }
+        }
     }
 }
