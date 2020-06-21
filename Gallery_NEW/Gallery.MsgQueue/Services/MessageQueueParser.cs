3e75d0bf935a3d970c0714cb729db92305da9ba9
BOGDAN_ZOLOTUKHIN_KB18 <b.zolotuxin@gmail.com>
2020-06-21 19:37:23 +0300

    Implemented 'MessageQueueParser' in Gallery.MsgQueue

---
+++ Gallery_NEW/Gallery.MsgQueue/Services/MessageQueueParser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gallery.MsgQueue.Services
+{
+    public static class MessageQueueParser
+    {
+        public static List<string> ParseMessageQueuePaths()
+        { 
+            var messageQueues = ConfigurationManager.AppSettings["msgQueue:paths"] ?? throw new ArgumentException("msgQueue:paths");
+
+            return messageQueues.Split(',').ToList();
+        }
+    }
+}
