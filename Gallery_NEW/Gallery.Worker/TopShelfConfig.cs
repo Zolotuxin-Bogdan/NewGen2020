8b1520f2dcdd1eafc6d0df28b02953d8742f13f6
BOGDAN_ZOLOTUKHIN_KB18 <b.zolotuxin@gmail.com>
2020-06-22 12:59:02 +0300

    Implemented 'TopShelfConfig' class

--- Gallery_NEW/Gallery.Worker/TopShelfConfig.cs
+++ Gallery_NEW/Gallery.Worker/TopShelfConfig.cs
@@ -1,12 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Topshelf;
 
 namespace Gallery.Worker
 {
-    class TopShelfConfig
+    internal static class TopShelfConfig
     {
+        internal static void Configure()
+        {
+            var uploadImageWork = Factory.GetUploadImageWork();
+
+            var exitCode = HostFactory.Run(configure =>
+            {
+                configure.Service<WorkerWrapper>(service =>
+                {
+                    service.ConstructUsing(s => new WorkerWrapper(uploadImageWork));
+                    service.WhenStarted(async s => await s.StartAsync());
+                    service.WhenStopped(s => s.Stop());
+                });
+
+                configure.RunAsLocalSystem();
+
+                configure.SetDescription("Service for Gallery Worker");
+                configure.SetDisplayName("Worker Wrapper Service");
+                configure.SetServiceName("WorkerWrapperService");
+            });
+
+            var exitCodeValue = (int) Convert.ChangeType(exitCode, exitCode.GetTypeCode());
+            Environment.ExitCode = exitCodeValue;
+        }
     }
 }
