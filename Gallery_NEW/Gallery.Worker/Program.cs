ac07445df8a3a12d553bad51b6a4cdd7d6c54700
BOGDAN_ZOLOTUKHIN_KB18 <b.zolotuxin@gmail.com>
2020-06-22 13:20:42 +0300

    Added message queue initialazing in Program.cs in Gallery.Worker project

--- Gallery_NEW/Gallery.Worker/Program.cs
+++ Gallery_NEW/Gallery.Worker/Program.cs
@@ -1,9 +1,14 @@
-﻿namespace Gallery.Worker
+﻿using Gallery.MsgQueue.Services;
+
+namespace Gallery.Worker
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var parseMessageQueue = MessageQueueParser.ParseMessageQueuePaths();
+            MessageQueueCreator.CreateMessageQueues(parseMessageQueue);
+
             TopShelfConfig.Configure();
         }
     }
