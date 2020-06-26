3b0fcb3af41581c5e4c2025f433dc222d0831fb0
BOGDAN_ZOLOTUKHIN_KB18 <b.zolotuxin@gmail.com>
2020-06-26 16:06:27 +0300

    Implemented RabbitMQPublisher

--- Gallery_NEW/Gallery.MsgQueue/Services/RabbitMQPublisher.cs
+++ Gallery_NEW/Gallery.MsgQueue/Services/RabbitMQPublisher.cs
@@ -1,12 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Configuration;
 using System.Text;
-using System.Threading.Tasks;
+using Gallery.MsgQueue.Interfaces;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
 
 namespace Gallery.MsgQueue.Services
 {
-    class RabbitMQPublisher
+    public class RabbitMQPublisher : IPublisher
     {
+        public void SendMessage<T>(T message, string messageQueuePath)
+        {
+            var connectionString = new Uri(ParseRabbitMQConnectionString());;
+            var factory = new ConnectionFactory() { Uri = connectionString };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    var messageBody = SerializeToBytes(SerializeToJson(message));
+
+                    channel.BasicPublish(exchange: "",
+                        routingKey: messageQueuePath,
+                        basicProperties: null,
+                        body: messageBody);
+                }
+            }
+        }
+
+        private static string ParseRabbitMQConnectionString()
+        {
+            var connectionString = ConfigurationManager.AppSettings["RabbitMQ:uri"] ?? throw new ArgumentException("RabbitMQ:uri");
+            return connectionString;
+        }
+
+        private static string SerializeToJson<T>(T obj) =>
+            JsonConvert.SerializeObject(obj);
+
+        private static byte[] SerializeToBytes(string obj) =>
+            Encoding.UTF8.GetBytes(obj);
     }
 }
