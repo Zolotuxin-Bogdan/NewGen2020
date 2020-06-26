614d6fa0d80ffc26d4c1b1f45072128801e3378e
BOGDAN_ZOLOTUKHIN_KB18 <b.zolotuxin@gmail.com>
2020-06-26 16:55:06 +0300

    Implemented RabbitMQConsumer

--- Gallery_NEW/Gallery.MsgQueue/Services/RabbitMQConsumer.cs
+++ Gallery_NEW/Gallery.MsgQueue/Services/RabbitMQConsumer.cs
@@ -1,12 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Configuration;
 using System.Text;
-using System.Threading.Tasks;
+using Gallery.MsgQueue.Interfaces;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
 namespace Gallery.MsgQueue.Services
 {
-    class RabbitMQConsumer
+    public class RabbitMQConsumer : IConsumer
     {
+        public T ReceiveFirstMessage<T>(string messageQueuePath)
+        {
+            var connectionString = new Uri(ParseRabbitMQConnectionString());
+            var factory = new ConnectionFactory() {Uri = connectionString };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    var result = channel.BasicGet(messageQueuePath, true);
+                    var messageBody = result.Body.ToArray();
+                    return DeserializeJson<T>(DeserializeBytes(messageBody));
+                }
+            }
+        }
+
+        private static string DeserializeBytes(byte[] obj) =>
+            Encoding.UTF8.GetString(obj);
+
+        private static T DeserializeJson<T>(string obj) =>
+            JsonConvert.DeserializeObject<T>(obj);
+
+        private static string ParseRabbitMQConnectionString()
+        {
+            var connectionString = ConfigurationManager.AppSettings["RabbitMQ:uri"] ??
+                                   throw new ArgumentException("RabbitMQ:uri");
+            return connectionString;
+        }
     }
 }
+
