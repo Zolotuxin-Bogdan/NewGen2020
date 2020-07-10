789259275fbe90991d8b2f6e2b8d458760441c14
BOGDAN_ZOLOTUKHIN_KB18 <b.zolotuxin@gmail.com>
2020-07-10 15:34:57 +0300

    Changed UploadImageWork implementation for input Action<T> type

--- Gallery_NEW/Gallery.Worker/Works/UploadImageWork.cs
+++ Gallery_NEW/Gallery.Worker/Works/UploadImageWork.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
-using FileStorageProvider.Interfaces;
 using Gallery.BLL;
 using Gallery.BLL.Contracts;
-using Gallery.DAL.Repositories.Interfaces;
 using Gallery.MsgQueue.Interfaces;
 using Gallery.MsgQueue.Services;
 using Gallery.Worker.Interfaces;
@@ -17,18 +14,14 @@ namespace Gallery.Worker.Works
     {
         private readonly IConsumer _consumer;
         private readonly IMediaService _mediaService;
-        private readonly IMediaRepository _mediaRepository;
-        private readonly IMediaStorageProvider _mediaStorage;
         private readonly CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
         private readonly TimeSpan _delayTime = TimeSpan.FromSeconds(1);
         private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
 
-        public UploadImageWork(IConsumer consumer, IMediaService mediaService, IMediaRepository mediaRepository, IMediaStorageProvider mediaStorage)
+        public UploadImageWork(IConsumer consumer, IMediaService mediaService)
         {
             _consumer = consumer;
             _mediaService = mediaService;
-            _mediaRepository = mediaRepository;
-            _mediaStorage = mediaStorage;
         }
 
         public async Task StartAsync()
@@ -36,36 +29,14 @@ namespace Gallery.Worker.Works
             var parseMessageQueueDictionary = MessageQueueParser.ParseMessageQueuePathsDictionary();
 
             while (!_cancellationTokenSource.IsCancellationRequested)
-            {
-                var messageDTO = _consumer.ReceiveFirstMessage<MessageDTO>(parseMessageQueueDictionary["upload-image"]);
-
-                if (!File.Exists(messageDTO.TempPath))
-                {
-                    throw new FileNotFoundException("File is missing:", messageDTO.TempPath);
-                }
-
-                var isTempMediaExist =
-                    await _mediaRepository.IsTempMediaExistByNameAndLoadingStatusAsync(messageDTO.FileName, true);
-
-                if (isTempMediaExist)
-                {
-                    var tempMedia =
-                        await _mediaRepository.GetTempMediaByNameAndLoadingStatusAsync(messageDTO.FileName, true);
-
-                    var newTempMedia = tempMedia;
-
-                    newTempMedia.IsLoading = false;
-                    newTempMedia.IsSuccess = true;
-
-                    var fileBytes = _mediaStorage.Read(messageDTO.TempPath);
-                    await _mediaService.UploadImageAsync(fileBytes, messageDTO.MainPath, messageDTO.UserId);
-
-                    await _mediaRepository.ChangeTempMediaAsync(tempMedia, newTempMedia);
-                }
-
-                Logger.Info("Started " + nameof(UploadImageWork) + ".");
-                await Task.Delay(_delayTime);
+            { 
+                _consumer.ConsumeFirstMessage<MessageDTO>(
+                    parseMessageQueueDictionary["upload-image"],
+                    async m => await _mediaService.MoveTempImageToUserDirectoryAsync(m));
             }
+
+            Logger.Info("Started " + nameof(UploadImageWork) + ".");
+            await Task.Delay(_delayTime);
         }
 
         public void Stop()
