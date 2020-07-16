65c800aec104c51764fc374cbd895d7070cb2fbd
BOGDAN_ZOLOTUKHIN_KB18 <b.zolotuxin@gmail.com>
2020-07-16 17:37:50 +0300

    Implemented AzureConsumer

--- Gallery_NEW/Gallery.MsgQueue.Azure/Implementation/AzureConsumer.cs
+++ Gallery_NEW/Gallery.MsgQueue.Azure/Implementation/AzureConsumer.cs
@@ -1,12 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
+using System.Threading;
+using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+using Gallery.MsgQueue.Interfaces;
+using Newtonsoft.Json;
 
 namespace Gallery.MsgQueue.Azure.Implementation
 {
-    class AzureConsumer
+    public class AzureConsumer : IConsumer
     {
+        private readonly TimeSpan _delayTime = TimeSpan.FromSeconds(1);
+
+        public void ConsumeFirstMessage<T>(string messageQueuePath, Action<T> action)
+        {
+            var connectionString = ParseAzureMQConnectionString();
+
+            var queueServiceClient = new QueueServiceClient(connectionString);
+            var queueClient = queueServiceClient.GetQueueClient(messageQueuePath);
+
+            while (true)
+            {
+                QueueProperties queueProperties = queueClient.GetProperties();
+
+                var msgCount = queueProperties.ApproximateMessagesCount;
+                if (msgCount > 0)
+                {
+                    QueueMessage[] receiveMessages = queueClient.ReceiveMessages(maxMessages: 1);
+
+                    var message = DeserializeJson<T>(receiveMessages[0].MessageText);
+
+                    queueClient.DeleteMessage(receiveMessages[0].MessageId, receiveMessages[0].PopReceipt);
+
+                    action(message);
+                    break;
+                }
+                Thread.Sleep(_delayTime);
+            }
+        }
+
+        private static string ParseAzureMQConnectionString()
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["AzureMQ:connectionString"].ConnectionString
+                                   ?? throw new ArgumentException("AzureMQ:connectionString");
+            return connectionString;
+        }
+
+        private static T DeserializeJson<T>(string obj) =>
+            JsonConvert.DeserializeObject<T>(obj);
     }
 }
